8b4742f0e50a49b66f7541fd3f97e13e4400313d
matt3209 <matthew.j.heider@gmail.com>
2022-03-17 21:16:39 -0500

    header content push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbCardModule, NbIconComponent, NbIconModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbCardModule, NbIconComponent, NbIconModule,NbSidebarModule, NbMenuModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { DashboardModule } from './dashboard/dashboard.module';
 
@@ -24,7 +24,9 @@ import { DashboardModule } from './dashboard/dashboard.module';
     NbEvaIconsModule,
     NbCardModule,
     DashboardModule,
-    NbIconModule
+    NbIconModule,
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
