3edda9697073d781edb25930313b3425f0a8b80b
craschka-dev <61559950+craschka-dev@users.noreply.github.com>
2022-03-26 19:35:24 -0500

    get wizard from api (in progress)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Wizard} from "../models/wizard";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,14 +8,39 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  testCall: any;
-  constructor(private http: HttpClient) { }
+  get currentWizard(): Wizard {
+    return this._currentWizard;
+  }
+
+  set currentWizard(value: Wizard) {
+    this._currentWizard = value;
+  }
+
+  private _currentWizard!: Wizard;
+  // wizardType: any;
+  // level: number;
+  // strength: number;
+  // speed: number;
+  // stamina: number;
+  // intelligence: number;
+  // spells: number;
+
+
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
-    this.http.get<any>('http://wizardsapi-env.eba-i9qbaatz.us-east-1.elasticbeanstalk.com/wizards/1').subscribe(data=>{
-      this.testCall = data.id;
-      console.log(this.testCall);
-    })
+    this.getWizard(1);
   }
+
+  getWizard(wizardId: number): void {
+    this.http.get<any>(`http://wizardsapi-env.eba-i9qbaatz.us-east-1.elasticbeanstalk.com/wizards/${wizardId}`).subscribe(data=>{
+      console.log(data);
+      let wiz: any = JSON.parse(data);
+      this._currentWizard wiz.map(() => {
+        });
+      });
+      console.log(this._currentWizard);
+    }
 
 }
---
+++ src/app/models/wizard.ts
@@ -0,0 +1,11 @@
+export interface Wizard {
+  id: number;
+  name: string;
+  profession: string;
+  strength: number;
+  speed: number;
+  stamina: number;
+  intelligence: number;
+  spells: number;
+  type: string;
+}
